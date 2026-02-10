c00074e78e957c570d683dcc886f6a313ae7a63e
Tarantula Trade <85387547+MAPSIM-co@users.noreply.github.com>
2026-02-10 18:52:18 +0330

    Update

--- my_financial_plan_backend/app/test_connection.py
+++
@@ -1,8 +0,0 @@
-from my_financial_plan_backend.app.db import Database
-
-conn, cursor = Database.connect()
-
-cursor.execute("SHOW TABLES;")
-tables = cursor.fetchall()
-
-print("Tables in database:", tables)
